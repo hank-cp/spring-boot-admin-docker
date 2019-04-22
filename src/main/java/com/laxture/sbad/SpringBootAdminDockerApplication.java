d00497d2cca8cab8734c8d37e86cdc953ca42a8c
Hank CP <true.cp@gmail.com>
2019-04-22 18:12:11 +0800

    Initial commit

---
+++ src/main/java/com/laxture/sbad/SpringBootAdminDockerApplication.java
@@ -0,0 +1,15 @@
+package com.laxture.sbad;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class SpringBootAdminDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laxture/sbad/SpringBootAdminDockerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.laxture.sbad;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootAdminDockerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
